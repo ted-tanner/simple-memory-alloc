fb20082c66e6475263d0ee90337a079b0196247e
Tanner Davies <tanner.e.davies@gmail.com>
2022-07-01 08:05:03 -0600

    Fix main to match new allocation scheme

--- src/main.c
+++ src/main.c
@@ -12,19 +12,17 @@ typedef struct {
 
 int main(int argc, char **argv)
 {
-    Page page;
-    page.flag = 42;
-    
     MemArena mem = create_arena(PAGE_SIZE * 12, PAGE_SIZE * 2);
-    Page *restrict struct1 = (Page*) alloc_struct(&mem, page);
+    Page *restrict struct1 = (Page*) alloc_struct(&mem, Page);
+    struct1->flag = 42;
 
-    page.flag = 43;
-    Page *restrict struct2 = (Page*) alloc_struct(&mem, page);
+    Page *restrict struct2 = (Page*) alloc_struct(&mem, Page);
+    struct2->flag = 43;
 
     for (int i = 0; i < 8; ++i)
-        alloc_struct(&mem, page);
+        alloc_struct(&mem, Page);
 
-    alloc_struct(&mem, page);
+    alloc_struct(&mem, Page);
 
     assert(struct1->flag == 42, "Flag is wrong");
     assert(struct2->flag == 43, "Flag is wrong");
